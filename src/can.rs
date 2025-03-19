96ad21ca46386f52a85a6dd51a645c3277401505
Maksim Savich <me@maksimsavich.com>
2025-03-19 08:12:25 +0000

    Working dbc commit

--- src/can.rs
+++ src/can.rs
@@ -25,6 +25,30 @@ impl CanDecoder {
             .iter()
             .find(|m| m.message_id().raw() == raw_id)?;
 
-        Some(message.message_name().to_string())
+        Some(
+            message
+                .signals()
+                .iter()
+                .fold(String::new(), |mut acc, signal| {
+                    let raw_value = {
+                        let data = frame.data();
+                        let start_bit = *signal.start_bit() as usize;
+                        let size = *signal.signal_size() as usize;
+                        let mut value = 0u64;
+
+                        for i in 0..size {
+                            let byte_index = (start_bit + i) / 8;
+                            let bit_index = (start_bit + i) % 8;
+                            if (data[byte_index] & (1 << bit_index)) != 0 {
+                                value |= 1 << i;
+                            }
+                        }
+                        value
+                    };
+                    let signal_value = (*signal.factor() * raw_value as f64) + *signal.offset();
+                    acc.push_str(&format!("{}: {}\n", signal.name(), signal_value));
+                    acc
+                }),
+        )
     }
 }
