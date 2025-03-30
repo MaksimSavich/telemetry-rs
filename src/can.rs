b523a9a9c795af90f371efab0d08413fc96e6b00
Maksim Savich <me@maksimsavich.com>
2025-03-30 21:04:12 +0000

    use little endian or big endian depending on the frame

--- src/can.rs
+++ src/can.rs
@@ -34,21 +34,22 @@ impl CanDecoder {
                         let data = frame.data();
                         let start_bit = *signal.start_bit() as usize;
                         let size = *signal.signal_size() as usize;
-                        let mut value = 0u64;
 
-                        for i in 0..size {
-                            let byte_index = (start_bit + i) / 8;
-                            let bit_index = (start_bit + i) % 8;
-                            if (data[byte_index] & (1 << bit_index)) != 0 {
-                                value |= 1 << i;
-                            }
-                        }
-                        value
+                        // Determine endianness from the DBC signal
+                        // In DBC format, Intel (little endian) is usually marked as "1"
+                        // Motorola (big endian) is usually marked as "0"
+                        let is_intel = match signal.byte_order() {
+                            can_dbc::ByteOrder::LittleEndian => true,
+                            can_dbc::ByteOrder::BigEndian => false,
+                        };
+
+                        self.extract_signal_value(data, start_bit, size, is_intel)
                     };
+
                     // Scale raw value to engineering value
                     let signal_value = (*signal.factor() * raw_value as f64) + *signal.offset();
 
-                    // Lookup value descriptions via DBC (not signal)
+                    // Lookup value descriptions via DBC
                     let value_desc = self
                         .dbc
                         .value_descriptions_for_signal(*message.message_id(), signal.name())
@@ -67,5 +68,51 @@ impl CanDecoder {
                     acc
                 }),
         )
+    }
+
+    // Helper method to extract signal value based on endianness
+    fn extract_signal_value(
+        &self,
+        data: &[u8],
+        start_bit: usize,
+        size: usize,
+        is_intel: bool,
+    ) -> u64 {
+        let mut value = 0u64;
+
+        if is_intel {
+            // Intel format (little-endian)
+            for i in 0..size {
+                let byte_index = (start_bit + i) / 8;
+                let bit_index = (start_bit + i) % 8;
+
+                // Make sure we don't go out of bounds
+                if byte_index < data.len() {
+                    if (data[byte_index] & (1 << bit_index)) != 0 {
+                        value |= 1 << i;
+                    }
+                }
+            }
+        } else {
+            // Motorola format (big-endian)
+            // In Motorola format, bits are numbered from MSB to LSB
+            for i in 0..size {
+                // Calculate the actual bit position in the CAN frame
+                let byte_index = start_bit / 8;
+                let bit_index = 7 - (start_bit % 8);
+                let current_bit = start_bit + i;
+                let current_byte = byte_index - (current_bit / 8 - byte_index);
+                let current_bit_in_byte = 7 - ((bit_index - i) % 8);
+
+                // Make sure we don't go out of bounds
+                if current_byte < data.len() {
+                    if (data[current_byte] & (1 << current_bit_in_byte)) != 0 {
+                        value |= 1 << (size - 1 - i);
+                    }
+                }
+            }
+        }
+
+        value
     }
 }
