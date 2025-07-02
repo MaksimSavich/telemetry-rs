36c7cb5e5392f58554cab74280ad2d3a1c509c80
Maksim Peter Savich <savichm2@msu.edu>
2025-07-02 17:42:10 -0500

    MPPT fix

--- src/gui.rs
+++ src/gui.rs
@@ -360,7 +360,7 @@ impl Application for TelemetryGui {
             {
                 let decoder = self.decoder.clone();
                 subscription::unfold("enhanced_can_subscription", decoder, |decoder| async {
-                    let socket = match CanSocket::open("vcan0") {
+                    let socket = match CanSocket::open("can0") {
                         Ok(s) => s,
                         Err(e) => {
                             eprintln!("Failed to open CAN socket: {}", e);
